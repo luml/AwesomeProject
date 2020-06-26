d2da4aba881a7bcb20000b7f6b2951991ff066ad
luml <1520622848@qq.com>
2020-06-26 12:27:27 +0800

    feat(components for colorBox)

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import ColorBox from './components/ColorBox';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
+    <SafeAreaView>
       <View style={styles.container}>
-        <Text>Hey There</Text>
+        <Text style={styles.text}>
+          Here are some boxes of different colours
+        </Text>
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
       </View>
     </SafeAreaView>
   );
@@ -13,13 +20,12 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'pink',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 40,
   },
-  safeArea: {
-    flex: 2,
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
